3da82a1cb40b4d2b87b1e64e98f29017d24200b6
sdan1 <sdjulios@ufpso.edu.co>
2025-04-09 20:34:49 -0500

    Consumiendo API(Cambio 2)

--- js/loginService.js
+++ js/loginService.js
@@ -31,3 +31,32 @@ document.getElementById("formLogin").addEventListener('submit', function(e){
     document.getElementById('alert').innerHTML = alert
 
 })
+
+function login(email, password) {
+    fetch("https://reqres.in/api/login" , {
+        method: "POST" , 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email,password})
+    })
+
+    .then((data)>={
+        alertType ='success'
+        message = 'Inicio de seccion exitoso.'
+        console.log('Responde bien' + data)
+    })
+    .catch((error) >={
+        alertType = 'danger'
+        message = 'Correo electronio o contraseña incorrectos'
+        console.error('Erro')
+
+    })
+     /* Creacion de la ventana de alerta*/
+     let alert= `
+     <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+         ${message}
+         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+     </div>
+ `;
+}
